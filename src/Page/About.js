fb09ccff0e74eac17a0eed93b8b19c23ec47fff8
foongzyy <foongzy@gmail.com>
2022-01-16 16:45:29 +0800

    refined about pg

--- src/Page/About.js
+++ src/Page/About.js
@@ -70,21 +70,21 @@ function About(props) {
                                                 <CardBody>
                                                     <div className="aboutBody">
                                                         <div className='container-fluid '>
-                                                            <label>TripSg is a personal full stack project that seeks to quickly provide users with their bus arrival information</label>
+                                                            <label>TripSg is a personal full stack project that seeks to quickly provide users with their bus arrival information.</label>
                                                         </div>
                                                         <div className='container-fluid '>
                                                             <h5 className='marA'><b>Features</b></h5>
                                                             <ul>
                                                                 <li>Search for bus stops using bus stop name, bus stop code or bus number</li>
                                                                 <li>Quicksearch enables you to quickly obtain bus arrival info near you</li>
-                                                                <li>Bus arrival information includes bus route bus capacity and wheelchair accessibility</li>
-                                                                <li>Ability to show bus stop location on map</li>
+                                                                <li>Bus arrival info includes bus route, bus capacity and wheelchair accessibility</li>
+                                                                <li>Show bus stop location on map</li>
                                                                 <li>Personalisation: <br />
                                                                     <ul>
                                                                         <li>Bookmarking commonly visited bus stops</li>
                                                                         <li>Starring bus services for bookmarked bus stops </li>                                                     
-                                                                        <li>Ability to change display name</li>
-                                                                        <li>Ability to change nearby radius</li>
+                                                                        <li>Change display name</li>
+                                                                        <li>Change nearby radius</li>
                                                                         <li>Dark Mode</li>
                                                                     </ul>
                                                                 </li>
@@ -92,9 +92,10 @@ function About(props) {
                                                         </div>
                                                         <div className='container-fluid '>
                                                             <h5 className='marA'><b>Source</b></h5>
-                                                            <label className='marC'><b>Bus info:</b> TripSg utilizes real time data from the Land Transport Authority of Singapore (LTA) </label>
+                                                            <label className='marC'><b>Bus info:</b> TripSg utilizes real time data from the Land Transport Authority of Singapore (LTA). </label>
                                                             <br />
-                                                            <label className='marC'><b>Code:</b> You can view the sourcecode <a href="https://github.com/foongzy/tripsg" target="_blank">here</a> </label>
+                                                            <br />
+                                                            <label className='marC'><b>Code:</b> You can view the sourcecode <a href="https://github.com/foongzy/tripsg" target="_blank">here</a>. </label>
                                                         </div>
                                                         <div className='container-fluid linee'>
                                                         <h5 className='marA'><b>Author</b></h5>
